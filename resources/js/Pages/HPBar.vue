96bcd000392ddf8757f32af3a8299f2058573ab3
xLink <xlink@cybershade.org>
2025-09-27 01:02:35 +0100

    updated HPBar

--- resources/js/Pages/HPBar.vue
+++ resources/js/Pages/HPBar.vue
@@ -45,14 +45,10 @@ export default {
     let channelString = ['App', 'HPBar', this.bar].join('.');
     window.Echo.channel(channelString)
       .listen('HPBar\\Update', (event) => {
-        console.log('UpdateBoard event received:', event);
         this.currentHP = parseInt(event.hpBar.current_hp);
         this.maxHP = parseInt(event.hpBar.max_hp);
-        console.log('Setting HP to ', this.currentHP, '/', this.maxHP);
       })
-      .listenToAll((event, data) => {
-        console.log('Uncaught event received:', event, data);
-      });
+    ;
       
   },
   
